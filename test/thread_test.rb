678653dc9a5e173a1253b3080ee6993afd8e436f
ken <block24block@gmail.com>
2017-03-13 10:45:25 +0800

    also test html for thread

--- test/thread_test.rb
+++ test/thread_test.rb
@@ -46,17 +46,14 @@ EOS
       @test.a
     end
 
-    #assert_equal ReturnValue, rt
-
     @sio.rewind
     assert_equal Tracetree, @sio.read
   end
 
-  #def test_trace_tree_html
-  #  rt = binding.trace_tree(html: true, tmp: 'normal.html', ignore: Ignore) do
-  #    @test.a
-  #  end
-  #  assert_equal ReturnValue, rt
-  #end
+  def test_trace_tree_html
+    rt = binding.trace_tree(html: true, tmp: 'thread.html', ignore: Ignore) do
+      @test.a
+    end
+  end
 
 end
