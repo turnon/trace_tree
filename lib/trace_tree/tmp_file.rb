1e64ee325408cfbe37ba4c86b0916ba0f1ed82c8
ken <block24block@gmail.com>
2017-05-22 22:34:47 +0800

    mkdir -p under /tmp/

--- lib/trace_tree/tmp_file.rb
+++ lib/trace_tree/tmp_file.rb
@@ -1,4 +1,5 @@
 require 'tmpdir'
+require 'fileutils'
 
 class TraceTree
   class TmpFile
@@ -27,11 +28,17 @@ class TraceTree
       path = Array(path).map(&:to_s)
       path[-1] = time + path[-1]
       path = [@tmp] + path
+      ensure_parent path
       File.join *path
     end
 
     def time
       Time.now.strftime '%Y%m%d_%H%M%S_%L_'
+    end
+
+    def ensure_parent path_arr
+      dir = path_arr[0..-2]
+      FileUtils.mkdir_p File.join *dir
     end
   end
 end
