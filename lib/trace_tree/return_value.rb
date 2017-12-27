cae5512b97eeaa962c7499f95ae7f2d41c31b82e
ken <block24block@gmail.com>
2017-12-28 00:01:49 +0800

    console.log the returned value

--- lib/trace_tree/return_value.rb
+++ lib/trace_tree/return_value.rb
@@ -1,7 +1,18 @@
 class TraceTree
   module ReturnValue
+
+    SHOW_RT = <<-JS
+TreeHtml.hover_press('r', function(n){
+  var a = n.querySelector('a');
+  console.log('>', a.querySelector('span').innerText + "\\n" + a.getAttribute('data-return'));
+});
+JS
     def data_for_tree_html
       super.merge!({return: ::JSON.generate([return_value])})
+    end
+
+    def body_js_for_tree_html
+      super.push({text: SHOW_RT})
     end
   end
 end
