37e1df0da197ac8782af250ec2a29e20a03fa5a8
ken <block24block@gmail.com>
2018-01-01 23:43:31 +0800

    pp returned values

--- lib/trace_tree/return_value.rb
+++ lib/trace_tree/return_value.rb
@@ -1,3 +1,6 @@
+require 'cgi'
+require 'pp'
+
 class TraceTree
   module ReturnValue
 
@@ -7,12 +10,37 @@ TreeHtml.hover_press('r', function(n){
   console.log('>', a.querySelector('span').innerText + "\\n" + a.getAttribute('data-return'));
 });
 JS
+
+    BLANK = ''.freeze
+
+    NEED_PP = (
+      if File.exists? (pp_config = File.join(ENV['HOME'], '.trace_tree_pp'))
+        IO.readlines(pp_config).map &:strip
+      else
+        []
+      end
+    ).freeze
+
     def data_for_tree_html
-      super.merge!({return: ::JSON.generate([return_value])})
+      super.merge!({return: formatted_return_value})
     end
 
     def body_js_for_tree_html
       super.push({text: SHOW_RT})
     end
+
+    private
+
+    def formatted_return_value
+      klass = ::Kernel.instance_method(:class).bind(return_value).call
+      value =
+        if klass <= Object && klass.ancestors.any?{ |a| NEED_PP.include? a.to_s }
+          ::PP.singleline_pp(return_value, BLANK.dup)
+        else
+          ::Kernel.instance_method(:to_s).bind(return_value).call
+        end
+      ::CGI.escapeHTML value
+    end
+
   end
 end
