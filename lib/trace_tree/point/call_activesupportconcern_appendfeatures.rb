8d5e66fabfbeab55d2910bf7177a2c8cc3b46831
ken <block24block@gmail.com>
2019-12-19 16:42:40 +0800

    activesupportconcern is optional

--- lib/trace_tree/point/call_activesupportconcern_appendfeatures.rb
+++ lib/trace_tree/point/call_activesupportconcern_appendfeatures.rb
@@ -1,17 +1,21 @@
-require 'active_support/concern'
+begin
+  require 'active_support/concern'
 
-class TraceTree
-  class Point
-    class CallActivesupportconcernAppendfeatures < Point
+  class TraceTree
+    class Point
+      class CallActivesupportconcernAppendfeatures < Point
 
-      def self.event_class_method
-        [:call, ActiveSupport::Concern, :append_features]
+        def self.event_class_method
+          [:call, ActiveSupport::Concern, :append_features]
+        end
+
+        def parameters
+          current.lv.values.first
+        end
+
       end
-
-      def parameters
-        current.lv.values.first
-      end
-
     end
   end
+rescue LoadError
+  nil
 end
