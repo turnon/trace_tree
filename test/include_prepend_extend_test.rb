6fc52ed828759dadcb7f8fc78ba6a36882993422
ken <block24block@gmail.com>
2019-12-24 17:25:06 +0800

    get around `No visible difference in the String#inspect output`

--- test/include_prepend_extend_test.rb
+++ test/include_prepend_extend_test.rb
@@ -46,48 +46,49 @@ class IncludePrependExtendTest < Minitest::Test
     end
   end
 
+  class C
+  end
+
   Mixin = -> do
-    Class.new do
-      include M
-      include N
-      prepend O
-      prepend P
-      extend R
-      extend Q
-    end
+    klass = Class.new C
+    klass.include M
+    klass.include N
+    klass.prepend O
+    klass.prepend P
+    klass.extend Q
+    klass.extend R
   end
 
   Tracetree = <<EOS
-IncludePrependExtendTest#block in test_trace_tree #{__dir__}/include_prepend_extend_test.rb:98
-└─IncludePrependExtendTest.block in <class:IncludePrependExtendTest> #{__dir__}/include_prepend_extend_test.rb:49
-  └─Class#new #{__dir__}/include_prepend_extend_test.rb:50
-    └─Class#initialize #{__dir__}/include_prepend_extend_test.rb:50
-      ├─Class#inherited #{__dir__}/include_prepend_extend_test.rb:50
-      └─#<Class:0xXXXXXX>.block (2 levels) in <class:IncludePrependExtendTest> #{__dir__}/include_prepend_extend_test.rb:50
-        ├─Module#include(IncludePrependExtendTest::M) #{__dir__}/include_prepend_extend_test.rb:51
-        │ ├─Module#append_features #{__dir__}/include_prepend_extend_test.rb:51
-        │ └─Module#included #{__dir__}/include_prepend_extend_test.rb:51
-        ├─Module#include(IncludePrependExtendTest::N) #{__dir__}/include_prepend_extend_test.rb:52
-        │ ├─IncludePrependExtendTest::N.append_features #{__dir__}/include_prepend_extend_test.rb:13
-        │ │ └─Module#append_features #{__dir__}/include_prepend_extend_test.rb:14
-        │ └─IncludePrependExtendTest::N.included #{__dir__}/include_prepend_extend_test.rb:9
-        │   └─Module#included #{__dir__}/include_prepend_extend_test.rb:10
-        ├─Module#prepend(IncludePrependExtendTest::O) #{__dir__}/include_prepend_extend_test.rb:53
-        │ ├─Module#prepend_features #{__dir__}/include_prepend_extend_test.rb:53
-        │ └─Module#prepended #{__dir__}/include_prepend_extend_test.rb:53
-        ├─Module#prepend(IncludePrependExtendTest::P) #{__dir__}/include_prepend_extend_test.rb:54
-        │ ├─IncludePrependExtendTest::P.prepend_features #{__dir__}/include_prepend_extend_test.rb:28
-        │ │ └─Module#prepend_features #{__dir__}/include_prepend_extend_test.rb:29
-        │ └─IncludePrependExtendTest::P.prepended #{__dir__}/include_prepend_extend_test.rb:24
-        │   └─Module#prepended #{__dir__}/include_prepend_extend_test.rb:25
-        ├─Kernel#extend(IncludePrependExtendTest::R) #{__dir__}/include_prepend_extend_test.rb:55
-        │ ├─IncludePrependExtendTest::R.extend_object #{__dir__}/include_prepend_extend_test.rb:43
-        │ │ └─Module#extend_object #{__dir__}/include_prepend_extend_test.rb:44
-        │ └─IncludePrependExtendTest::R.extended #{__dir__}/include_prepend_extend_test.rb:39
-        │   └─Module#extended #{__dir__}/include_prepend_extend_test.rb:40
-        └─Kernel#extend(IncludePrependExtendTest::Q) #{__dir__}/include_prepend_extend_test.rb:56
-          ├─Module#extend_object #{__dir__}/include_prepend_extend_test.rb:56
-          └─Module#extended #{__dir__}/include_prepend_extend_test.rb:56
+IncludePrependExtendTest#block in test_trace_tree #{__dir__}/include_prepend_extend_test.rb:99
+└─IncludePrependExtendTest.block in <class:IncludePrependExtendTest> #{__dir__}/include_prepend_extend_test.rb:52
+  ├─Class#new #{__dir__}/include_prepend_extend_test.rb:53
+  │ └─Class#initialize #{__dir__}/include_prepend_extend_test.rb:53
+  │   └─Class#inherited #{__dir__}/include_prepend_extend_test.rb:53
+  ├─Module#include(IncludePrependExtendTest::M) #{__dir__}/include_prepend_extend_test.rb:54
+  │ ├─Module#append_features #{__dir__}/include_prepend_extend_test.rb:54
+  │ └─Module#included #{__dir__}/include_prepend_extend_test.rb:54
+  ├─Module#include(IncludePrependExtendTest::N) #{__dir__}/include_prepend_extend_test.rb:55
+  │ ├─IncludePrependExtendTest::N.append_features #{__dir__}/include_prepend_extend_test.rb:13
+  │ │ └─Module#append_features #{__dir__}/include_prepend_extend_test.rb:14
+  │ └─IncludePrependExtendTest::N.included #{__dir__}/include_prepend_extend_test.rb:9
+  │   └─Module#included #{__dir__}/include_prepend_extend_test.rb:10
+  ├─Module#prepend(IncludePrependExtendTest::O) #{__dir__}/include_prepend_extend_test.rb:56
+  │ ├─Module#prepend_features #{__dir__}/include_prepend_extend_test.rb:56
+  │ └─Module#prepended #{__dir__}/include_prepend_extend_test.rb:56
+  ├─Module#prepend(IncludePrependExtendTest::P) #{__dir__}/include_prepend_extend_test.rb:57
+  │ ├─IncludePrependExtendTest::P.prepend_features #{__dir__}/include_prepend_extend_test.rb:28
+  │ │ └─Module#prepend_features #{__dir__}/include_prepend_extend_test.rb:29
+  │ └─IncludePrependExtendTest::P.prepended #{__dir__}/include_prepend_extend_test.rb:24
+  │   └─Module#prepended #{__dir__}/include_prepend_extend_test.rb:25
+  ├─Kernel#extend(IncludePrependExtendTest::Q) #{__dir__}/include_prepend_extend_test.rb:58
+  │ ├─Module#extend_object #{__dir__}/include_prepend_extend_test.rb:58
+  │ └─Module#extended #{__dir__}/include_prepend_extend_test.rb:58
+  └─Kernel#extend(IncludePrependExtendTest::R) #{__dir__}/include_prepend_extend_test.rb:59
+    ├─IncludePrependExtendTest::R.extend_object #{__dir__}/include_prepend_extend_test.rb:43
+    │ └─Module#extend_object #{__dir__}/include_prepend_extend_test.rb:44
+    └─IncludePrependExtendTest::R.extended #{__dir__}/include_prepend_extend_test.rb:39
+      └─Module#extended #{__dir__}/include_prepend_extend_test.rb:40
 EOS
 
   def setup
